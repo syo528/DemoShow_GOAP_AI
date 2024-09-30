45b359272b15fa0f18566eeb06b95e0be803790a
Joker <you@example.com739554159@qq.com>
2024-09-30 22:27:46 +0800

    15.计划与计划节点

--- Assets/Scripts/GOAP/GOAPAgent.cs
+++ Assets/Scripts/GOAP/GOAPAgent.cs
@@ -7,6 +7,8 @@ public class GOAPAgent : SerializedMonoBehaviour
     [LabelText("目标")] public GOAPGoals goals;
     [LabelText("局部状态")] public GOAPStates states;
     [LabelText("全部行为")] public GOAPActions actions;
+    [LabelText("计划")] public GOAPPlan plan;
+
     public IGOAPOwner owner { get; private set; }
     public void Init(IGOAPOwner owner)
     {
